f45159d0b86be1a3253adf6c6ab40dde35be0076
Gavin Grant <gavingrant@gmail.com>
2020-07-02 14:16:24 -0700

    created form, list, and item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React from "react";
+import AddItemForm from './AddItemForm';
+import ShoppingList from './ShoppingList';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+  state = {
+    shoppingItems: [
+      /* put stub items in here for testing */
+      { name: 'apples', checked: false },
+      { name: 'oranges', checked: true },
+      { name: 'bread', checked: false },
+    ]
+  };
+
+  handleDeleteItem = (item) => {
+    const newItems = this.state.shoppingItems.filter(itm => itm !== item)
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+
+  handleCheckItem = (item) => {
+    const newItems = this.state.shoppingItems.map(itm => {
+      if (itm === item) {
+        itm.checked = !itm.checked
+      }
+      return itm
+    })
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+
+  handleAddItem = (itemName) => {
+    const newItems = [
+      ...this.state.shoppingItems,
+      { name: itemName, checked: false }
+    ]
+    this.setState({
+      shoppingItems: newItems
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <h1>Shopping List</h1>
+        </header>
+        <main>
+          <section>
+            <AddItemForm 
+              onAddItem={this.handleAddItem} 
+            />
+          </section>
+          <section>
+            <ShoppingList 
+              items={this.state.shoppingItems} 
+              onDeleteItem={this.handleDeleteItem}
+              onCheckItem={this.handleCheckItem}
+            />
+          </section>
+        </main>
+      </>
+    )
+  }
 }
 
 export default App;
